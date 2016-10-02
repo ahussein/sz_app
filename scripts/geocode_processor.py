ec5cf730c99e24ef105d83d04bba519095d83672
Abdelrahman Hussein <ahussein@codescalers.com>
2016-10-02 21:56:46 +0200

    unicode

--- scripts/geocode_processor.py
+++ scripts/geocode_processor.py
@@ -22,10 +22,71 @@ The script will extract the Handlungsort field values and process them to clean 
 a pair of latitude and longitude. An example Handlungsort field value is: Lausitzring, Klettwitz, Deutschland
 """
 
-import csv
+import csv, codecs, cStringIO
 import os
 import sys
 import geocoder
+
+# handle unicode issues
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
 
 class Error(Exception):
 	"""
@@ -85,10 +146,10 @@ def main(input_file_path):
 	result = {}
 	geocoder = GeocoderFactory().get()
 	with open(input_file_path, 'rb') as fd:
-		reader = csv.DictReader(fd, delimiter=';')
+		reader = UnicodeReader(fd, delimiter=';')
 		for row in reader:
-			location_address = row['Handlungsorte']
-			article_id = row['DialogId']
+			location_address = row[-2]
+			article_id = row[0]
 			if not location_address:
 				print('Artical [%s] does not have location set' % article_id)
 			geocoder_result = geocoder(location_address)
