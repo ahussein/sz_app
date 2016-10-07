e20e6337bb189723b3c653f330cb3fec2ca21968
Abdelrahman Hussein <ahussein@codescalers.com>
2016-10-07 19:08:33 +0200

    use mongodb-org-unstable package

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -4,7 +4,7 @@ sudo su -
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.3 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
-apt-get install -y mongodb-org
+apt-get install -y mongodb-org-unstable
 
 cat > /lib/systemd/system/mongod.service <<EOF
 [Unit]
