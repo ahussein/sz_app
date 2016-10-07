589f0d5231f7bb58a1c643a9e5530b34c3af1d69
Abdelrahman Hussein <ahussein@codescalers.com>
2016-10-07 20:41:04 +0200

    update install script

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo su -
+# install mongodb
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.3 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
@@ -22,3 +24,11 @@ WantedBy=multi-user.target
 EOF
 
 /usr/bin/mongod --quiet --config /etc/mongod.conf&
+
+# install pip and virtualenv
+apt-get install -y python-pip
+pip install --upgrade pip
+pip install virtualenv
+
+cd /opt/sz_app/app
+env/bin/pip install -r requirements.txt
