d5de409a0b3d498314acf67604ae0d72a5973d53
Abdelrahman Hussein <ahussein@codescalers.com>
2016-10-26 17:00:57 +0200

    update for wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI engry point of the backend app
 """
-from app import app
+from app.app import app
 
 if __name__ == "__main__":
-    app.app.run(debug=True, host='0.0.0.0' ,port=8080)
+    app.run(debug=True, host='0.0.0.0' ,port=8080)
