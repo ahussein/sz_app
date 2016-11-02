7e07a55abc384b3499f745805e4e9d78d024d7dc
Abdelrahman Hussein <ahussein@codescalers.com>
2016-11-02 03:06:57 +0200

    add artile type as feature

--- app/app.py
+++ app/app.py
@@ -70,11 +70,12 @@ class Article(Resource):
 			article['distance'] = _calculate_distance(article['address']['coordinates'], location_filter['source'])
 			
 			# geojson require a properties attribute, we need to do this better!
+			for key, value in article.copy().iteritems():
+				article['properties'][key] = value
 			article['geometry'] = {}
 			if 'geometry' in article['address']:
 				article['geometry'] = article['address']['geometry']
-			for key, value in article.copy().iteritems():
-				article['properties'][key] = value
+			article['type'] = 'Feature'
 
 		result = {'response': found_articles if found_articles else 'No articles found', 'count': len(found_articles)}
 		return mongo_jsonfy(result)
